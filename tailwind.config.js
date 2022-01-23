1187a7275de12611503ea88a85643d9dc3bb2714
Rėjus Černiauskas <rejusvaldo123@google.com>
2022-01-23 12:41:26 +0200

    Projekto sukurimas

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      // colors: {
+      //   primary: '#202225',
+      //   secondary: '#5865f2',
+      // }
+    },
+  },
+  plugins: [],
+}
