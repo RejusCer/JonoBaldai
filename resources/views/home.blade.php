17ba09ea97b8903be6f2efdb7b967e65cdd54056
Rėjus Černiauskas <rejusvaldo123@google.com>
2022-01-24 13:25:47 +0200

    pabaigtos navigacijos juostos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layout/mainLayout')
 
 @section('main')
-    MAIN
+    {{-- Jei ekranas mažas: kategorijas rodys pagrindiniame lauke --}}
+    <div class="flex flex-wrap justify-evenly md:hidden mt-8 ">
+        <a href="#" class="category-container"><i class="fas fa-align-justify"></i> <p class="ml-1">Kėdės</p></a>
+        <a href="#" class="category-container"><i class="fas fa-archive"></i> <p class="ml-1">Komodos</p></a>
+        <a href="#" class="category-container"><i class="fas fa-align-justify"></i> <p class="ml-1">Spintos</p></a>
+        <a href="#" class="category-container"><i class="fas fa-wrench"></i> <p class="ml-1">Įrankiai</p></a>
+        <a href="#" class="category-container"><i class="fas fa-align-justify"></i> <p class="ml-1">Stalai</p></a>
+        <a href="#" class="category-container"><i class="fas fa-credit-card"></i> <p class="ml-1">su akcija</p></a>
+    </div>
 @endsection
--- resources/views/layout/mainLayout.blade.php
+++ resources/views/layout/mainLayout.blade.php
@@ -14,8 +14,8 @@
 </head>
 <body>
     
-    <div class="bg-primary w-2/12 fixed top-0 h-full pt-4 pl-4">
-        <a href="#" class="border-r-2 border-black font-bold text-2xl pr-2 hidden md:inline">JonoBaldai</a>
+    <div class="bg-primary w-2/12 fixed h-full pt-4 pl-4 hidden md:block">
+        <a href="#" class="border-r-2 border-black font-bold text-xl pr-2">JonoBaldai</a>
         
         <div class="flex flex-col mt-8">
             <a href="#" class="sidebar-buttons"><i class="fas fa-align-justify"></i> <p class="sidebar-text">Kėdės</p></a>
@@ -23,25 +23,31 @@
             <a href="#" class="sidebar-buttons"><i class="fas fa-align-justify"></i> <p class="sidebar-text">Spintos</p></a>
             <a href="#" class="sidebar-buttons"><i class="fas fa-wrench"></i> <p class="sidebar-text">Įrankiai</p></a>
             <a href="#" class="sidebar-buttons"><i class="fas fa-align-justify"></i> <p class="sidebar-text">Stalai</p></a>
-            <a href="#" class="sidebar-buttons"><i class="fas fa-credit-card"></i> <p class="sidebar-text">Išpardavimai</p></a>
+            <a href="#" class="sidebar-buttons"><i class="fas fa-credit-card"></i> <p class="sidebar-text">su akcija</p></a>
         </div>
     </div>
     
 
-    <div class="ml-[16.7%]">
-        <nav>
-            <div class="bg-secondary w-full flex justify-between items-center">
-                <div class="flex">
+    <div class="md:ml-[16.7%]">
+        <nav class="bg-secondary">
+            <div class=" w-full flex flex-col sm:flex-row justify-between items-center">
+                <div class="flex self-start sm:self-center">
+                    <a href="#" class="border-r-2 border-black font-bold text-xl px-2 self-center inline md:hidden">JB</a>
                     <a href="#" class="top-nav-items">Susisiekime</a>
                     <a href="#" class="top-nav-items">Apie</a>
                 </div>
-                <div class="flex items-center">
-                    <a href="#" class="top-nav-items"><i class="fas fa-sign-in-alt"></i> Prisijungti</a>
+                <a href="#" class="toggler absolute top-3 right-4 flex sm:hidden flex-col justify-between w-8 h-5">
+                    <span class="toggle-button"></span>
+                    <span class="toggle-button"></span>
+                    <span class="toggle-button"></span>
+                </a>
+                <div class="nav-buttons hidden sm:flex items-center">
+                    <a href="#" class="top-nav-items"><i class="fas fa-sign-in-alt"></i> <span class="hidden sm:inline">Prisijungti</span></a>
                     <a href="#" class="top-nav-items border-4 rounded-3xl border-black p-2 hover:bg-quatiary">
-                        <i class="fas fa-shopping-cart"></i> Krepšelis 0
+                        <i class="fas fa-shopping-cart"></i> <span class="hidden sm:inline">Krepšelis </span>0
                     </a>
                     <a href="#" class="top-nav-items border-4 rounded-3xl border-black p-2 hover:bg-quatiary">
-                        <i class="fas fa-heart"></i> Norai 0
+                        <i class="fas fa-heart"></i> <span class="hidden sm:inline">Norai </span>0
                     </a>
                 </div>
             </div>
@@ -50,5 +56,15 @@
 
         @yield('main')
     </div>
+
+    <script>
+        const toggler = document.querySelectorAll(".toggler")[0]
+        const buttons = document.querySelectorAll(".nav-buttons")[0]
+
+        toggler.addEventListener('click', () => {
+            buttons.classList.toggle("hidden")
+            buttons.classList.toggle("flex")
+        })
+    </script>
 </body>
 </html>
